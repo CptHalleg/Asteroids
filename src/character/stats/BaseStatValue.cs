5afd6c0358ac1e84e82d39e3aba1d5c30e56b10f
Lukas <mr.halleg@gmail.com>
2025-04-01 03:07:19 +0200

    seperated out character files

--- src/character/stats/BaseStatValue.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-[GlobalClass]
-public partial class BaseStatValue : Resource
-{
-	[Export] public AttributeStatType AttributeStatType{get; private set;}
-	[Export] public float Value{get; private set;}
-}
--- src/character/stats/stattypes/AddAttributeStatType.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-[GlobalClass]
-public partial class AddAttributeStatType : AttributeStatType{
-    public override Stat NewStat(StatBlock statBlock)
-    {
-        return new AddAttributeStat(this, statBlock);
-    }
-}
